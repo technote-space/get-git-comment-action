acab4f132893dfaf4eb1d764f2c2003ab9180a8b
Technote <technote.space@gmail.com>
2023-05-30 00:40:16 +0900

    chore: tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
-import crypto from 'crypto';
 import { resolve } from 'path';
 import { setFailed } from '@actions/core';
 import { Context } from '@actions/github/lib/context';
 import { ContextHelper } from '@technote-space/github-action-helper';
 import { Logger } from '@technote-space/github-action-log-helper';
 import { execute } from './process';
-
-if (typeof global.crypto !== 'object') {
-  global.crypto = crypto;
-}
-
-if (typeof global.crypto.getRandomValues !== 'function') {
-  global.crypto.getRandomValues = getRandomValues;
-}
-
-function getRandomValues(array) {
-  return crypto.webcrypto.getRandomValues(array);
-}
 
 const run = async(): Promise<void> => {
   const logger  = new Logger();
